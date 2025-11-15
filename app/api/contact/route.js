8a8cc9b1ae485f1ca1f522178c9baf4bb40f47b7
Ibrahim <syedibrahim5436@gmail.com>
2025-11-15 19:57:15 +0500

    resolve resend error

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -2,6 +2,8 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+
+
 export async function POST(req) {
   try {
     const { name, email, message } = await req.json();
